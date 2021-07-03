3aaf1ba46a0e37cffe2801ff76131bc4e92f7a9f
Teo-KJ <48685014+Teo-KJ@users.noreply.github.com>
2021-07-03 23:41:17 +0800

    Remove some stuff

---
+++ Main Program/MapData.py
@@ -0,0 +1,33 @@
+import os
+from functions import *
+
+class MapData:
+    
+    def __init__(self, name, coordinates, period, satellite):
+        self.name = name
+        self.coordinates = coordinates
+        self.period = period
+        self.satellite = satellite
+
+    def getData(self):
+        # swtich the coordinates placement
+        polygon = switchCoordinates(self.coordinates)
+        
+        # satellite missions
+        sat_list = [self.satellite]
+        
+        # name of the site
+        sitename = f'{self.name}' + ' ' + str(sat_list[0])
+        
+        # directory where the data will be stored
+        filepath = os.path.join(os.getcwd(), 'extracted data')
+        
+        # put all the inputs into a dictionnary
+        inputs = {'polygon': polygon, 'dates': self.period, 'sat_list': sat_list, 'sitename': sitename, 'filepath':filepath}
+        
+        return inputs
+    
+    def getArea(self):
+        length = self.coordinates[0][0][0] - self.coordinates[0][2][0]
+        width = self.coordinates[0][1][1] - self.coordinates[0][0][1]
+        return length * width
