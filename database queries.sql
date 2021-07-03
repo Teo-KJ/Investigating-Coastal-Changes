df5fac7d6b72d00d6acca11f4e5d338bb602e63a
Teo-KJ <48685014+Teo-KJ@users.noreply.github.com>
2021-07-03 23:17:26 +0800

    Made changes

--- database queries.sql
+++ database queries.sql
@@ -7,51 +7,52 @@ CREATE TABLE shorelineData (
 	geoaccuracy FLOAT NOT NULL,
 	idx SMALLINT NOT NULL,
 	satname VARCHAR NOT NULL
-);
+)
 
 CREATE TABLE polygonData (
 	location VARCHAR NOT NULL,
 	polygon VARCHAR NOT NULL,
 	folder_name VARCHAR NOT NULL
-);
+)
 
-ALTER TABLE shorelineData RENAME COLUMN date TO dates;
+ALTER TABLE shorelineData RENAME COLUMN date TO dates
 
-DROP TABLE polygonData;
-
-DELETE FROM shorelineData
-WHERE location = 'Cuddalore, India';
+DROP TABLE polygonData
 
 DELETE FROM shorelineData
-WHERE date > '2020-01-01';
---SET date = date || '+00:00';
+WHERE location = 'Cuddalore, India'
+
+DELETE FROM shorelineData
+WHERE date > '2020-01-01'
+--SET date = date || '+00:00'
 
 UPDATE shorelineData
 SET date = LEFT(date, LENGTH(date)-6)
-WHERE LENGTH(date)>19;
+WHERE LENGTH(date)>19
 
 INSERT INTO polygonData
-VALUES ('TLW and TTW, Hong Kong', '%s', 'Hong Kong');
+VALUES ('TLW and TTW, Hong Kong', '%s', 'Hong Kong')
 
 ALTER TABLE polygonData 
-ADD COLUMN folder_name VARCHAR;
+ADD COLUMN folder_name VARCHAR
 
 UPDATE polygonData SET folder_name = 'San Jose'
-WHERE location = 'San Jose, Philippines';
+WHERE location = 'San Jose, Philippines'
 
 UPDATE shorelineData
 SET location = 'TLW and TTW, Hong Kong'
-WHERE location = 'HK TLW and TTW';
+WHERE location = 'HK TLW and TTW'
 
 SELECT * 
 FROM shorelineData 
-ORDER BY location, dates;
+ORDER BY location, dates
 
 SELECT * 
 FROM shorelineData 
 WHERE location = 'Tanauan, Philippines' 
-ORDER BY dates;
+ORDER BY dates
 
 SELECT DISTINCT location 
-FROM shorelineData;
+FROM shorelineData
 
+SELECT * FROM polygonData
